82e30f6d05d70c645dba2f779681798e729727ff
natalia <nataliamelendez2426@gmail.com>
2025-11-10 04:08:17 -0500

    Auto-sync: file changes

--- basedatos/models.py
+++ basedatos/models.py
@@ -321,6 +321,17 @@ class GarantiaArchivo(db.Model):
     RutaArchivo = db.Column(db.String(500), nullable=False)
     FechaSubida = db.Column(db.DateTime, default=datetime.utcnow)
 
+class GarantiaProducto(db.Model):
+    __tablename__ = 'GarantiaProducto'
+
+    ID_GarantiaProducto = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    ID_Garantia = db.Column(db.Integer, db.ForeignKey('Garantia.ID_Garantia', ondelete='CASCADE'), nullable=False)
+    ID_Producto = db.Column(db.Integer, db.ForeignKey('Producto.ID_Producto'), nullable=False)
+
+    garantia = db.relationship('Garantia', backref='productos_garantia')
+    producto = db.relationship('Producto')
+
+
 # ------------------ Etiqueta ------------------
 class Etiqueta(db.Model):
     __tablename__ = 'Etiqueta'
